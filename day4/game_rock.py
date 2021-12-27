8df0b533829514a8808056ccad036d1ede92686b
Jazmin <jppinedac940@hotmail.com>
2021-12-27 15:05:26 -0300

    (iteracion bucles for y while)

--- day4/game_rock.py
+++ day4/game_rock.py
@@ -1,61 +1,88 @@
 import random
 
-print("Wellcom games rock, paper and scissonrs")
-game_one= int(input("what do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissonrs "))
+def print_jugada(jugador):
+    rock= '''
 
-rock= '''
-
-        ,--.--._
-------" _, \___)
-        / _/____)
-        \//(____)
-------\     (__)
-       `-----"
- 
-    '''
-paper= '''
-               ___..__
-  __..--""" ._ __.'
-              "-..__
-            '"--..__";
- ___        '--...__"";
-    `-..__ '"---..._;"
-          """"----'   
-    '''
-
-sissors= '''
+            ,--.--._
+    ------" _, \___)
+            / _/____)
+            \//(____)
+    ------\     (__)
+        `-----"
     
-    .-.  _
-    | | / )
-    | |/ /
-   _|__ /_
-  / __)-' )
-  \  `(.-')
-   > ._>-'
-  / \/
+        '''
+    paper= '''
+                ___..__
+    __..--""" ._ __.'
+                "-..__
+                '"--..__";
+    ___        '--...__"";
+        `-..__ '"---..._;"
+            """"----'   
+        '''
 
-    '''
+    sissors= '''
+        
+        .-.  _
+        | | / )
+        | |/ /
+    _|__ /_
+    / __)-' )
+    \  `(.-')
+    > ._>-'
+    / \/
+
+        '''
+    games={"0":rock, "1":paper, "2":sissors}
+    for clave in games:
+        if clave == jugador:
+            print(games[clave])
+   
+       
+
+print("Wellcom games rock, paper and scissonrs")
+game_one= str(input("what do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissonrs "))  
+while int(game_one) < 0 or int(game_one) >=3:
+    print("You type an invalid number, you lose!")
+    game_one= str(input("what do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissonrs "))  
+
+print_jugada(game_one)
+game_two= str(random.randint(0,2))
+print(f"Jugada PC elige: {game_two}")
+print_jugada(game_two)
+
+    
+if game_one == "0" and game_two=="2":
+    print("You win")
+elif game_two =="0" and game_one=="2":
+    print("You lose")
+elif game_two > game_one: # fijese que se pone de primera la computadora porque el puntaje cumple si es mayor q
+    print("You lose")
+elif game_one > game_two:
+    print("You win")
+elif game_one == game_two:
+    print("It`s a draw")
+else:
+    print("You type an invalid number, you lose!")
+
+#Una manera de hacerlo
+# if game_one == "0" and game_two=="2":
+#     print("You win")
+# elif game_one == "2" and game_two=="1":
+#     print("You win")
+# elif game_one == "1" and game_two=="0":
+#     print("You win")
+# elif game_one == game_two:
+#     print("it`s a draw")
+# else:
+#     print("You lost, computer win!")
 
 
-games=[rock, paper, sissors]
-
-for i in range(0, len(games)):
-    if i == game_one:
-        print(games[i])
-game_two= random.randint(0,2)
-print(game_two)
-for i in range(0, len(games)):
-    if i == game_two:
-        print("Computer chose:")
-        print(games[i])
 
 
 
-lose= "you lose"
-draw= "you had a draw"  
-win = "you win"
-puntaje = [lose, draw, win ]
-for k in range(0, len(puntaje)):
-    print(puntaje[k])
+
+
+
     
     
---
+++ day5/loops_range_codblock.py
@@ -0,0 +1,35 @@
+#Prueba de listas de frutas y ejecuciòn con un for
+# fruits=["Piniapple","Peach", "Pear", "Apple"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " "+" Pie")
+#lista1= [180, 124, 165, 173, 189, 169, 146]
+# lista2=[]
+# student_heights=0
+# total=0
+# contar=0
+
+# salir = "no"
+# while salir == "no":
+#     student_heights = int(input("Input a list of student heights "))
+#     lista2.append(student_heights)
+#     contar +=1 
+#     total = student_heights + total
+#     salir= input("Desea salir? (si o no)")
+
+# promed = round(total/ contar)
+# print(lista2, contar, total, promed)
+student_heights = input("Input a list of student heights ").split()
+print(type (student_heights), student_heights)
+for n in range(0, len(student_heights)):
+   student_heights[n] = int(student_heights[n])
+
+contador = 0
+sumar=0
+for n in range(0, len(student_heights)):
+   sumar = student_heights[n] + sumar
+   contador +=1
+promedio= round(sumar/contador)
+ 
+print(sumar, contador, promedio)
+
