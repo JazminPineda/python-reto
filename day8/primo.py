25ea68740e82a68481c39a1f3f5e8843512515b6
Jazmin <jppinedac940@hotmail.com>
2022-01-26 21:56:23 -0300

    prueba

--- day8/primo.py
+++ day8/primo.py
@@ -3,6 +3,7 @@ import math
 def prime_checker(number):
     if number/number == 1 and number/1== number:
         print("It's a prime number.")
+        "finde"
     else:
         print("It's not a prime number.")
 
